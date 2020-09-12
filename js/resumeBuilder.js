41b9d45d8cd81814c8a619189abbbb7942f204d1
Osakpolor Obaseki <obasekiosa@gmail.com>
2020-09-12 08:27:29 +0100

    first commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,135 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+//var formattedName = HTMLheaderName.replace("%data%", "Osakpolor Obaseki");
+//var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
+//
+//$("#header").prepend(formattedName, formattedRole);
+
+
+var bio = {
+    "name": "Osakpolor Obaseki",
+    "role": "Software Engineer",
+    "contacts": {
+        "email": "obasekiosa@gmail.com",
+        "mobile": "+2348162831365",
+        "github": "Sazi2178",
+        "twitter": "obasekiosa",
+        "linkedin": "obasekiosa",
+        "location": "Lagos Nigeria"
+    },
+    "bio_pic": "images/fry.jpg",
+    "welcome_message": "Hello there nice to meet you",
+    "skills": [
+        "awesomeness",
+        "programming",
+        "teaching",
+        "Js"
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "",
+            "dates": "",
+            "description": ""
+        },
+        
+        {
+            "title": "",
+            "dates": "",
+            "description": ""
+        },
+        
+        {
+            "title": "",
+            "dates": "",
+            "description": ""
+        }
+    ],
+    
+    "images": [
+        "url",
+        "url"
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": ""
+        },
+        
+        {
+            "employer": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": ""
+        },
+        
+        {
+            "employer": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": ""
+        }
+    ]
+    
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "University of Lagos",
+            "location": "Lagos",
+            "dates": "2019",
+            "degree": "Electrical and Electronic Engineering",
+            "majors": [
+                "None"
+            ]
+        },
+        
+        {
+            "name": "Baptist High School Makurdi",
+            "location": "Makurdi",
+            "dates": "2014",
+            "degree": "High School Diploma",
+            "majors": [
+                "None"
+            ]
+        }
+    ],
+    
+    "online_courses": [
+        {
+            "platform": "Edx",
+            "host": "MIT",
+            "title": "6.00.1x: Introduction to Computational Science and Programming with Python",
+            "dates": "2019",
+            "url": ""
+        },
+        
+        {
+            "platform": "Udacity",
+            "host": "Udacity",
+            "title": "Javascript Basics",
+            "dates": "2019",
+            "url": ""
+        },
+        
+        {
+            "platform": "Cousera",
+            "host": "University of Jerusalem",
+            "title": "Nand to Tetris: Building a Computer from Start to Finish",
+            "dates": "2019",
+            "url": ""
+        }
+    ]
+};
